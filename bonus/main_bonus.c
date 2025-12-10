90f902d225ebd07522546173ff49ebfbcd6566b9
sorayaafdz <159011568+sorayaafdz@users.noreply.github.com>
2025-12-10 18:38:44 +0100

    Add files via upload

---
+++ bonus/main_bonus.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sofernan <sofernan@student.42madrid.es>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/13 14:19:57 by vdiez-cu          #+#    #+#             */
+/*   Updated: 2025/12/10 18:36:22 by sofernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+int	load_texture(void *mlx, t_texture *texture, char *path)
+{
+	texture->imagen = mlx_xpm_file_to_image(mlx, path,
+			&texture->width, &texture->height);
+	if (!texture->imagen)
+		return (write(2, "Error\nTexture load failed\n", 26), -1);
+	texture->addr = mlx_get_data_addr(texture->imagen, &texture->bit_by_pixel,
+			&texture->line_len_byte, &texture->endian);
+	return (0);
+}
+
+void	free_image(void *mlx, void **img_ptr, char **addr_ptr)
+{
+	if (!mlx || !img_ptr)
+		return ;
+	if (*img_ptr)
+	{
+		mlx_destroy_image(mlx, *img_ptr);
+		*img_ptr = NULL;
+		if (addr_ptr)
+			*addr_ptr = NULL;
+	}
+}
+
+void	free_config(t_game *g)
+{
+	if (!g)
+		return ;
+	if (g->mlx)
+	{
+		free_image(g->mlx, &g->texture_no.imagen, &g->texture_no.addr);
+		free_image(g->mlx, &g->texture_so.imagen, &g->texture_so.addr);
+		free_image(g->mlx, &g->texture_we.imagen, &g->texture_we.addr);
+		free_image(g->mlx, &g->texture_ea.imagen, &g->texture_ea.addr);
+		free_image(g->mlx, &g->frame_img, &g->frame_addr);
+		if (g->window)
+			(mlx_destroy_window(g->mlx, g->window), g->window = NULL);
+		(mlx_destroy_display(g->mlx), free(g->mlx), g->mlx = NULL);
+	}
+	if (g->no)
+		(free(g->no), g->no = NULL);
+	if (g->so)
+		(free(g->so), g->so = NULL);
+	if (g->we)
+		(free(g->we), g->we = NULL);
+	if (g->ea)
+		(free(g->ea), g->ea = NULL);
+}
+
+void	init_player(t_game *g, char orient)
+{
+	g->dirx = 0.0;
+	g->diry = 0.0;
+	g->planex = 0.0;
+	g->planey = 0.0;
+	if (orient == 'N')
+	{
+		g->diry = -1.0;
+		g->planex = 0.66;
+	}
+	else if (orient == 'S')
+	{
+		g->diry = 1.0;
+		g->planex = -0.66;
+	}
+	else if (orient == 'E')
+	{
+		g->dirx = 1.0;
+		g->planey = 0.66;
+	}
+	else if (orient == 'W')
+	{
+		g->dirx = -1.0;
+		g->planey = -0.66;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_game		g;
+	char		orient;
+
+	if (argc != 2)
+		return (write(2, "Error\nArguments\n", 16), -1);
+	if (parse(argv, &g, &orient) < 0)
+		return (1);
+	if (load_texture(g.mlx, &g.texture_no, g.no) < 0 || load_texture(g.mlx,
+			&g.texture_so, g.so) < 0 || load_texture(g.mlx, &g.texture_we,
+			g.we) < 0 || load_texture(g.mlx, &g.texture_ea, g.ea) < 0)
+		return (free_map(&g), free_config(&g), 1);
+	g.window = mlx_new_window(g.mlx, g.screen_w, g.screen_h, "cub3D");
+	if (!g.window)
+		return (perror("Error\nWindow\n"), free_map(&g), free_config(&g), 1);
+	g.posx = (double)g.init_player_x + 0.5;
+	g.posy = (double)g.init_player_y + 0.5;
+	init_player(&g, orient);
+	if (create_frame_image(&g) < 0)
+		return (free_map(&g), free_config(&g), 1);
+	mlx_hook(g.window, 2, 1, key_press, &g);
+	mlx_hook(g.window, 3, 2, key_release, &g);
+	mlx_hook(g.window, 17, 0, handle_close, &g);
+	mlx_hook(g.window, 6, 64, mouse_move, &g);
+	(mlx_loop_hook(g.mlx, game_loop, &g), mlx_loop(g.mlx));
+	return (free_map(&g), free_config(&g), 0);
+}
---
+++ bonus/player_movement_bonus.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player_movement_bonus.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sofernan <sofernan@student.42madrid.es>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 16:53:40 by sofernan          #+#    #+#             */
+/*   Updated: 2025/12/10 18:36:25 by sofernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+void	move_forward_back(t_game *g, double move_speed, int direction)
+{
+	double	next_x;
+	double	next_y;
+
+	next_x = g->posx + g->dirx * move_speed * direction;
+	next_y = g->posy + g->diry * move_speed * direction;
+	if (next_y >= 0 && next_y < g->map_height && (int)g->posx >= 0
+		&& (int)g->posx < g->map_width && g->map[(int)next_y][(int)g->posx] != '1'
+		&& g->map[(int)next_y][(int)g->posx] != ' ')
+		g->posy = next_y;
+	if ((int)g->posy >= 0 && (int)g->posy < g->map_height && next_x >= 0
+		&& next_x < g->map_width && g->map[(int)g->posy][(int)next_x] != '1'
+		&& g->map[(int)g->posy][(int)next_x] != ' ')
+		g->posx = next_x;
+}
+
+void	move_left_right(t_game *g, double move_speed, int direction)
+{
+	double	next_x;
+	double	next_y;
+
+	next_x = g->posx + g->planex * move_speed * direction;
+	next_y = g->posy + g->planey * move_speed * direction;
+	if (next_y >= 0 && next_y < g->map_height && (int)g->posx < g->map_width
+		&& (int)g->posx >= 0 && g->map[(int)next_y][(int)g->posx] != '1'
+		&& g->map[(int)next_y][(int)g->posx] != ' ')
+		g->posy = next_y;
+	if ((int)g->posy >= 0 && (int)g->posy < g->map_height && next_x >= 0
+		&& next_x < g->map_width && g->map[(int)g->posy][(int)next_x] != '1'
+		&& g->map[(int)g->posy][(int)next_x] != ' ')
+		g->posx = next_x;
+}
+
+void	rotate_player(t_game *g, double angle)
+{
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = g->dirx;
+	g->dirx = g->dirx * cos(angle) - g->diry * sin(angle);
+	g->diry = old_dir_x * sin(angle) + g->diry * cos(angle);
+	old_plane_x = g->planex;
+	g->planex = g->planex * cos(angle) - g->planey * sin(angle);
+	g->planey = old_plane_x * sin(angle) + g->planey * cos(angle);
+}
+
+void	update_player(t_game *g, double delta)
+{
+	double	move_speed;
+	double	rot_speed;
+
+	move_speed = 3.0 * delta;
+	rot_speed = 2.0 * delta;
+	if (g->key_w)
+		move_forward_back(g, move_speed, +1);
+	if (g->key_s)
+		move_forward_back(g, move_speed, -1);
+	if (g->key_a)
+		move_left_right(g, move_speed, -1);
+	if (g->key_d)
+		move_left_right(g, move_speed, +1);
+	if (g->key_left)
+		rotate_player(g, -rot_speed);
+	if (g->key_right)
+		rotate_player(g, rot_speed);
+}
